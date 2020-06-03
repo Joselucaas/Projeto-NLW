2a07340e3279aa5740b1478b8b558602ceea14e7
Joselucaas <lucas.chag0608@gmail.com>
2020-06-03 10:17:45 -0300

    Página de cadastro 99%

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,5 +1,4 @@
-
-
+//Estados e cidades
 
 function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
@@ -46,9 +45,55 @@ function getCities(event) {
         
     })
 }
-
-
-
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+//ítens de coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    //Adicionar ou remover uma classe
+    itemLi.classList.toggle("selected")
+    
+    const itemId = event.target.dataset.id 
+
+    // Verificar se existem itens selecionados, se sim
+    // pegar os itens selecionados
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId
+        return itemFound
+    })
+
+    //se já estiver selecionado,
+    if( alreadySelected >= 0) {
+        // tirar da seleção
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+        // se não estiver selecionado
+        // adicionar à seleção
+        selectedItems.push(itemId)
+    }
+
+    // atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+
+} 
